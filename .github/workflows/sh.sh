94949515fa390693691d553545f1f94b956aade7
nimid <ss.saroj@gmail.com>
2021-10-02 14:29:34 +0700

    Add .github/workflows/sh.sh

---
+++ .github/workflows/sh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/95.0.4638.17/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
